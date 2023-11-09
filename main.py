f0b556e605a52496aa12b54aaf276bb6de5af882
Christian Hey <c.hey@sbs-herzogenaurach.de>
2023-11-09 12:52:16 +0100

    Solved task 1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-row_parkingspace = list()
+def set_parkingspace(row):
+    number_plate = input("Bitte geben Sie Ihr Kennzeichen an: ")
+    position = int(input("Bitte geben Sie die Parkplatznummer an: "))
+
+    row[position - 1] = number_plate
+
+parkingspace = [""] * 6
+
+set_parkingspace(parkingspace)
+print(parkingspace)
