375861a6974772f972d6358d3c0c62e418e5ee71
Christian Hey <c.hey@sbs-herzogenaurach.de>
2023-11-09 12:56:10 +0100

    Solved task 2

--- main.py
+++ main.py
@@ -4,7 +4,15 @@ def set_parkingspace(row):
 
     row[position - 1] = number_plate
 
+def free_parkingspace(row):
+    position = int(input("Bitte geben Sie die Parkplatznummer an: "))
+
+    row[position - 1] = ""
+
 parkingspace = [""] * 6
 
 set_parkingspace(parkingspace)
+print(parkingspace)
+
+free_parkingspace(parkingspace)
 print(parkingspace)
