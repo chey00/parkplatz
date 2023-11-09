84d00c1e1ee25e9695681b0f1d4bdba08dac0e53
Christian Hey <c.hey@sbs-herzogenaurach.de>
2023-11-09 13:43:44 +0100

    Solved task 5 - task 4 is missing.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def set_parkingspace(row):
+from datetime import datetime
+
+def set_parkingspace(row, time):
     number_plate = input("Bitte geben Sie Ihr Kennzeichen an: ")
     position = int(input("Bitte geben Sie die Parkplatznummer an: "))
 
@@ -8,10 +10,11 @@ def set_parkingspace(row):
 
     if row[position - 1] == "":
         row[position - 1] = number_plate
+        time[position - 1] = datetime.now()
     else:
         print("Parkplatz bereits besetzt")
 
-def free_parkingspace(row):
+def free_parkingspace(row, time):
     position = int(input("Bitte geben Sie die Parkplatznummer an: "))
 
     if position < 1 or position > 6:
@@ -21,12 +24,22 @@ def free_parkingspace(row):
     if row[position - 1] == "":
         print("Parkplatz ist bereits leer")
     else:
+        current_time = datetime.now()
+        delta = current_time - time[position - 1]
+
+        seconds = delta.total_seconds()
+        print("Sie haben", seconds, "Sekunden geparkt.")
+
         row[position - 1] = ""
+        time[position - 1] = None
 
 parkingspace = [""] * 6
+parkingtime = [None] * 6
 
-set_parkingspace(parkingspace)
-print(parkingspace)
+set_parkingspace(parkingspace, parkingtime)
+print("Parkplätze", parkingspace)
+print("Parkzeit", parkingtime)
 
-free_parkingspace(parkingspace)
-print(parkingspace)
+free_parkingspace(parkingspace, parkingtime)
+print("Parkplätze", parkingspace)
+print("Parkzeit", parkingtime)
