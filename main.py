e5254b2a7517503d515fcb109f635d98942d47d8
Christian Hey <c.hey@sbs-herzogenaurach.de>
2023-11-09 13:08:27 +0100

    Solved task 3

--- main.py
+++ main.py
@@ -14,12 +14,16 @@ def set_parkingspace(row):
 def free_parkingspace(row):
     position = int(input("Bitte geben Sie die Parkplatznummer an: "))
 
-    row[position - 1] = ""
+    if position < 1 or position > 6:
+        print("Parkplatz exsistiert nicht")
+        return
+
+    if row[position - 1] == "":
+        print("Parkplatz ist bereits leer")
+    else:
+        row[position - 1] = ""
 
 parkingspace = [""] * 6
-
-set_parkingspace(parkingspace)
-print(parkingspace)
 
 set_parkingspace(parkingspace)
 print(parkingspace)
